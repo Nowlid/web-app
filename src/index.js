4489840e033dfd989d7358a72ccc5b59902e8eb2
Hecone <angloamir@gmail.com>
2023-04-21 20:53:51 +0200

    Fix version + bad import in index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import pkg from 'body-parser';
 const { urlencoded, json } = pkg;
 import Database from 'better-sqlite3';
 import { route } from './routers.js';
-import emailUtils from './Utils/email.utils.js';
 
 new Database('src/Database/users.db').prepare(`CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT, avatar TEXT, dateCreated TEXT)`).run();
 
